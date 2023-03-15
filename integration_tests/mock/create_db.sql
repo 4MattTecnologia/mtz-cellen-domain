b4be08bb8f48e30820c1c35690545c8afd4af5c1
henrique.furst <henrique.furst@4matt.com.br>
2023-03-15 08:30:21 -0300

    [FEAT] PSQL integration tests successful

--- integration_tests/mock/create_db.sql
+++ integration_tests/mock/create_db.sql
@@ -16,3 +16,36 @@ create table origin_instances (
     domain_id integer,
     connection_values jsonb
 );
+create table stakeholders (
+    stakeholder_id serial primary key,
+    stakeholder_name varchar(32),
+    domain_ids integer[]
+);
+create table agreements (
+    agreement_id serial primary key,
+    agreement_name varchar(32),
+    num_mtz_users integer,
+    num_monitored_users integer,
+    page_limit integer
+);
+create table modules (
+    module_id serial primary key,
+    module_name varchar(32)
+);
+create table profiles (
+    profile_id serial primary key,
+    profile_name varchar(32),
+    security jsonb
+);
+create table mtz_users (
+    user_id serial primary key,
+    user_name varchar(32),
+    password varchar(32),
+    domain_id integer,
+    stakeholder_id integer,
+    profile_id integer,
+    start_date date,
+    end_date date,
+    private_key bytea,
+    public_key bytea
+);
