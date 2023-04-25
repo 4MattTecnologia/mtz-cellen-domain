4da29edeb27a900e7a698b14153c476de7cfce21
Rafael Silva <rafael.silva.ext@4matt.com.br>
2023-04-25 10:31:21 -0300

    [CHORE] - Testing push to GITHUB

--- model/domain.go
+++ model/domain.go
@@ -1,24 +1,26 @@
 package model
+
 import (
-    "fmt"
+	"fmt"
 )
 
 type Domain struct {
-    id          int
-    name        string
+	id   int
+	name string
 }
 
 func (d *Domain) GetId() int {
-    return d.id
+	println(d.id)
+	return d.id
 }
 
 func (d *Domain) GetName() string {
-    return d.name
+	return d.name
 }
 
 func NewDomain(id int, name string) (Domain, error) {
-    if name == "" {
-        return Domain{}, fmt.Errorf("Invalid empty name for domain")
-    }
-    return Domain{id, name}, nil
+	if name == "" {
+		return Domain{}, fmt.Errorf("Invalid empty name for domain")
+	}
+	return Domain{id, name}, nil
 }
